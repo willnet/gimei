e6299c19a19e885ea043a0e3390b442f9b048d43
willnet <netwillnet@gmail.com>
2013-06-03 23:44:19 +0900

    Add hiragana and katakana methods

--- lib/faker_japanese/name.rb
+++ lib/faker_japanese/name.rb
@@ -1,69 +1,70 @@
 # -*- coding:utf-8 -*
 module FakerJapanese
   module Name
-    extend self
     @first_names = [
-      ["樹", 'イツキ'],
-      ["大和", 'ヤマト'],
-      ["太一", 'タイチ'],
-      ["蒼空", 'アオゾラ'],
-      ["翼", 'ツバサ'],
-      ["陽菜", 'ハルナ'],
-      ["結衣", 'ユイ'],
-      ["杏", 'アンズ'],
-      ["莉子", 'リコ'],
-      ["美羽", 'ミウ'],
-      ["結菜", 'ユイナ'],
-      ["心愛", 'ココア'],
-      ["愛菜", 'アイナ'],
-      ["美咲", 'ミサキ']
+      ['樹', 'いつき', 'イツキ'],
+      ['大和', 'やまと', 'ヤマト'],
+      ['太一', 'たいち', 'タイチ'],
+      ['蒼空', 'あおぞら', 'アオゾラ'],
+      ['翼', 'つばさ', 'ツバサ'],
+      ['陽菜', 'はるな', 'ハルナ'],
+      ['結衣', 'ゆい', 'ユイ'],
+      ['杏', 'あんず', 'アンズ'],
+      ['莉子', 'りこ', 'リコ'],
+      ['美羽', 'みう', 'ミウ'],
+      ['結菜', 'ゆいな', 'ユイナ'],
+      ['心愛', 'ここあ', 'ココア'],
+      ['愛菜', 'あいな', 'アイナ'],
+      ['美咲', 'みさき', 'ミサキ']
     ]
     @last_names = [
-      ["佐藤", 'サトウ'],
-      ["鈴木", 'スズキ'],
-      ["高橋", 'タカハシ'],
-      ["田中", 'タナカ'],
-      ["渡辺", 'ワタナベ'],
-      ["伊藤", 'イトウ'],
-      ["山本", 'ヤマモト'],
-      ["中村", 'ナカムラ'],
-      ["小林", 'コバヤシ'],
-      ["加藤", 'カトウ'],
-      ["吉田", 'ヨシダ'],
-      ["山田", 'ヤマダ'],
-      ["佐々木", 'ササキ'],
-      ["山口", 'ヤマグチ'],
-      ["斎藤", 'サイトウ'],
-      ["松本", 'マツモト'],
-      ["井上", 'イノウエ'],
-      ["木村", 'キムラ'],
-      ["林", 'ハヤシ'],
-      ["清水", 'シミズ']
+      ['佐藤', 'さとう', 'サトウ'],
+      ['鈴木', 'すずき', 'スズキ'],
+      ['高橋', 'たかはし', 'タカハシ'],
+      ['田中', 'たなか', 'タナカ'],
+      ['渡辺', 'わたなべ', 'ワタナベ'],
+      ['伊藤', 'いとう', 'イトウ'],
+      ['山本', 'やまもと', 'ヤマモト'],
+      ['中村', 'なかむら', 'ナカムラ'],
+      ['小林', 'こばやし', 'コバヤシ'],
+      ['加藤', 'かとう', 'カトウ'],
+      ['吉田', 'よしだ', 'ヨシダ'],
+      ['山田', 'やまだ', 'ヤマダ'],
+      ['佐々木', 'ささき', 'ササキ'],
+      ['山口', 'やまぐち', 'ヤマグチ'],
+      ['斎藤', 'さいとう', 'サイトウ'],
+      ['松本', 'まつもと', 'マツモト'],
+      ['井上', 'いのうえ', 'イノウエ'],
+      ['木村', 'きむら', 'キムラ'],
+      ['林', 'はやし', 'ハヤシ'],
+      ['清水', 'しみず', 'シミズ']
     ]
 
+    class << self
       def name
         first, last = first_name, last_name
         name = [last, first].join(' ')
-        set_yomi(name, last.yomi + ' ' + first.yomi)
+        set_yomi(name, "#{last.hiragana} #{first.hiragana}", "#{last.katakana} #{first.katakana}")
         name
       end
 
-      [:first_name, :last_name].each do |m|
-        define_method m do
-          chosen = rand(instance_variable_get("@#{m}s"))
-          name = chosen[0]
-          set_yomi(name, chosen[1])
-          name
-        end
+      def first_name
+        set_yomi(*@first_names.sample)
       end
 
-      def set_yomi(name, yomi)
-        name.instance_variable_set('@yomi', yomi)
-        def name.yomi; @yomi end
+      def last_name
+        set_yomi(*@last_names.sample)
+      end
+
+      def set_yomi(name, hiragana, katakana)
+        name.define_singleton_method(:hiragana) { hiragana }
+        name.define_singleton_method(:katakana) { katakana }
+        name
       end
 
       def rand(array)
         array[Kernel.rand(array.size)]
       end
     end
+  end
 end
