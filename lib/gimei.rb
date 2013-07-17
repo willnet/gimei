b9bd2eedb154134959193fba1bf818308a0346ea
willnet <netwillnet@gmail.com>
2013-07-17 14:11:48 +0900

    Add new APIs and refactor

--- lib/gimei.rb
+++ lib/gimei.rb
@@ -5,78 +5,15 @@ class Gimei
   NAMES = YAML.load_file(File.expand_path(File.join('..', 'data', 'names.yml'), __FILE__))
   GENDER = [:male, :female].freeze
 
-  class First
-    def initialize(gender = nil)
-      @gender = gender || GENDER[rand 2]
-      @name = NAMES['first_name'][@gender.to_s].sample
-    end
-
-    def gender
-      @gender
-    end
-
-    def kanji
-      @name[0]
-    end
-
-    def hiragana
-      @name[1]
-    end
-
-    def katakana
-      @name[2]
-    end
-  end
-
-  class Last
-    def initialize
-      @name = NAMES['last_name'].sample
-    end
-
-    def kanji
-      @name[0]
-    end
-
-    def hiragana
-      @name[1]
-    end
-
-    def katakana
-      @name[2]
-    end
-  end
-
-  def initialize(gender = nil)
-    @gender = gender || GENDER[rand 2]
-    @first = First.new @gender
-    @last = Last.new
-  end
-
-  def gender
-    @gender
-  end
-
-  def kanji
-    "#{last.kanji} #{first.kanji}"
-  end
-
-  def hiragana
-    "#{last.hiragana} #{first.hiragana}"
-  end
-
-  def katakana
-    "#{last.katakana} #{first.katakana}"
-  end
-
-  def first
-    @first
-  end
-
-  def last
-    @last
-  end
-
   class << self
+    def male
+      new(:male)
+    end
+
+    def female
+      new(:female)
+    end
+
     def kanji
       name = new
       "#{name.last.kanji} #{name.first.kanji}"
@@ -98,6 +35,107 @@ class Gimei
 
     def last
       new.last
+    end
+  end
+
+  def initialize(gender = nil)
+    @gender = gender || GENDER.sample
+    @first = First.new @gender
+    @last = Last.new
+  end
+
+  def kanji
+    "#{last.kanji} #{first.kanji}"
+  end
+
+  def hiragana
+    "#{last.hiragana} #{first.hiragana}"
+  end
+
+  def katakana
+    "#{last.katakana} #{first.katakana}"
+  end
+
+  def to_s
+    kanji
+  end
+
+  def first
+    @first
+  end
+
+  def last
+    @last
+  end
+
+  def male?
+    @gender == :male
+  end
+
+  def female?
+    @gender == :female
+  end
+
+  class First
+    class << self
+      def male
+        new(:male)
+      end
+
+      def female
+        new(:female)
+      end
+    end
+
+    def initialize(gender = nil)
+      @gender = gender || GENDER.sample
+      @name = NAMES['first_name'][@gender.to_s].sample
+    end
+
+    def male?
+      @gender == :male
+    end
+
+    def female?
+      @gender == :female
+    end
+
+    def kanji
+      @name[0]
+    end
+
+    def hiragana
+      @name[1]
+    end
+
+    def katakana
+      @name[2]
+    end
+
+    def to_s
+      kanji
+    end
+  end
+
+  class Last
+    def initialize
+      @name = NAMES['last_name'].sample
+    end
+
+    def kanji
+      @name[0]
+    end
+
+    def hiragana
+      @name[1]
+    end
+
+    def katakana
+      @name[2]
+    end
+
+    def to_s
+      kanji
     end
   end
 end
--- spec/name_spec.rb
+++ spec/name_spec.rb
@@ -2,76 +2,95 @@
 require_relative 'spec_helper'
 
 describe Gimei do
-  before { @name = Gimei.new }
+  describe '.male' do
+    before { @name = Gimei.male }
+
+    it '男性の名前が返ること' do
+      @name.male?.must_equal true
+    end
+  end
+
+  describe '.female' do
+    before { @name = Gimei.female }
+
+    it '女性の名前が返ること' do
+      @name.female?.must_equal true
+    end
+  end
 
   describe '#kanji' do
+    before { @name = Gimei.new }
+
     it '全角文字とスペースが返ること' do
       @name.kanji.must_match /\A[#{Moji.zen}\s]+\z/
     end
   end
 
   describe '#hiragana' do
+    before { @name = Gimei.new }
+
     it 'ひらがなとスペースが返ること' do
       @name.hiragana.must_match /\A[\p{hiragana}\s]+\z/
     end
   end
 
   describe '#katakana' do
+    before { @name = Gimei.new }
+
     it 'カタカナとスペースが返ること' do
       @name.katakana.must_match /\A[\p{katakana}\s]+\z/
-    end
-  end
-
-  describe '#female' do
-    before { @name = Gimei.new(:female) }
-    it '女性の名前が返ること' do
-      assert_equal @name.first.gender, :female
-    end
-  end
-
-  describe '#male' do
-    before { @name = Gimei.new(:male) }
-    it '男性の名前が返ること' do
-      assert_equal @name.first.gender, :male
     end
   end
 end
 
 describe Gimei::First do
-  before { @name = Gimei::First.new }
+  describe '.male' do
+    before { @name = Gimei::First.male }
+
+    it '男性の名前が返ること' do
+      @name.male?.must_equal true
+    end
+  end
+
+  describe '.female' do
+    before { @name = Gimei::First.female }
+
+    it '女性の名前が返ること' do
+      @name.female?.must_equal true
+    end
+  end
 
   describe '#kanji' do
+    before { @name = Gimei::First.new }
+
     it '全角文字が返ること' do
       @name.kanji.must_match /\A#{Moji.zen}+\z/
     end
   end
 
   describe '#hiragana' do
+    before { @name = Gimei::First.new }
+
     it 'ひらがなが返ること' do
       @name.hiragana.must_match /\A\p{hiragana}+\z/
     end
   end
 
   describe '#katakana' do
+    before { @name = Gimei::First.new }
+
     it 'カタカナが返ること' do
       @name.katakana.must_match /\A\p{katakana}+\z/
     end
   end
 
-  describe '#female' do
-    before { @name = Gimei::First.new(:female) }
-    it '女性の名前が返ること' do
-      assert_equal @name.gender, :female
+  describe '#to_s' do
+    before { @name = Gimei::First.new }
+
+    it '全角文字が返ること' do
+      @name.to_s.must_match /\A#{Moji.zen}+\z/
     end
   end
-
-  describe '#male' do
-    before { @name = Gimei::First.new(:male) }
-    it '男性の名前が返ること' do
-      assert_equal @name.gender, :male
-    end
-  end
-
 end
 
 describe Gimei::Last do
@@ -92,6 +111,12 @@ describe Gimei::Last do
   describe '#katakana' do
     it 'カタカナが返ること' do
       @name.katakana.must_match /\A\p{katakana}+\z/
+    end
+  end
+
+  describe '#to_s' do
+    it '全角文字が返ること' do
+      @name.to_s.must_match /\A#{Moji.zen}+\z/
     end
   end
 end
