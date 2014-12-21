adeb8b7573420cf8d4c38207ceb3821ba450bfbe
willnet <netwillnet@gmail.com>
2014-12-21 23:54:33 +0900

    Add and refactor specs

---
+++ spec/gimei_spec.rb
@@ -0,0 +1,136 @@
+# coding: utf-8
+require_relative 'spec_helper'
+
+describe Gimei do
+  describe '.male' do
+    before { @name = Gimei.male }
+
+    it 'Gimei::Name オブジェクトが返ること' do
+      @name.must_be_instance_of Gimei::Name
+    end
+
+    it '#male? が true を返すこと' do
+      @name.male?.must_equal true
+    end
+  end
+
+  describe '.female' do
+    before { @name = Gimei.female }
+
+    it 'Gimei::Name オブジェクトが返ること' do
+      @name.must_be_instance_of Gimei::Name
+    end
+
+    it '#female? が true を返すこと' do
+      @name.female?.must_equal true
+    end
+  end
+
+  describe '#kanji' do
+    it '全角文字とスペースが返ること' do
+      Gimei.new.kanji.must_match /\A[#{Moji.zen}\s]+\z/
+    end
+  end
+
+  describe '#to_s' do
+    it '全角文字とスペースが返ること' do
+      Gimei.new.to_s.must_match /\A[#{Moji.zen}\s]+\z/
+    end
+  end
+
+  describe '.kanji' do
+    it '全角文字とスペースが返ること' do
+      Gimei.kanji.must_match /\A[#{Moji.zen}\s]+\z/
+    end
+  end
+
+  describe '#hiragana' do
+    it 'ひらがなとスペースが返ること' do
+      Gimei.new.hiragana.must_match /\A[\p{hiragana}\s]+\z/
+    end
+  end
+
+  describe '.hiragana' do
+    it 'ひらがなとスペースが返ること' do
+      Gimei.hiragana.must_match /\A[\p{hiragana}\s]+\z/
+    end
+  end
+
+  describe '#katakana' do
+    it 'カタカナとスペースが返ること' do
+      Gimei.new.katakana.must_match /\A[\p{katakana}\s]+\z/
+    end
+  end
+
+  describe '.katakana' do
+    it 'カタカナとスペースが返ること' do
+      Gimei.katakana.must_match /\A[\p{katakana}\s]+\z/
+    end
+  end
+
+  describe '.name' do
+    it 'Gimei::Name オブジェクトが返ること' do
+      Gimei.name.must_be_instance_of Gimei::Name
+    end
+  end
+
+  describe '#name' do
+    it 'Gimei::Name オブジェクトが返ること' do
+      Gimei.new.name.must_be_instance_of Gimei::Name
+    end
+  end
+
+  describe '.first' do
+    it 'Gimei::Name::First オブジェクトが返ること' do
+      Gimei.first.must_be_instance_of Gimei::Name::First
+    end
+  end
+
+  describe '#first' do
+    it 'Gimei::Name::First オブジェクトが返ること' do
+      Gimei.new.first.must_be_instance_of Gimei::Name::First
+    end
+  end
+
+  describe '.last' do
+    it 'Gimei::Name::First オブジェクトが返ること' do
+      Gimei.last.must_be_instance_of Gimei::Name::Last
+    end
+  end
+
+  describe '#last' do
+    it 'Gimei::Name::First オブジェクトが返ること' do
+      Gimei.new.last.must_be_instance_of Gimei::Name::Last
+    end
+  end
+
+  describe '.address' do
+    it 'Gimei::Address オブジェクトが返ること' do
+      Gimei.address.must_be_instance_of Gimei::Address
+    end
+  end
+
+  describe '#address' do
+    it 'Gimei::Address オブジェクトが返ること' do
+      Gimei.new.address.must_be_instance_of Gimei::Address
+    end
+  end
+
+  describe '.prefecture' do
+    it 'Gimei::Address::Prefecture オブジェクトが返ること' do
+      Gimei.prefecture.must_be_instance_of Gimei::Address::Prefecture
+    end
+  end
+
+  describe '.city' do
+    it 'Gimei::Address::City オブジェクトが返ること' do
+      Gimei.city.must_be_instance_of Gimei::Address::City
+    end
+  end
+
+  describe '.town' do
+    it 'Gimei::Address::Town オブジェクトが返ること' do
+      Gimei.town.must_be_instance_of Gimei::Address::Town
+    end
+  end
+end
--- spec/name_spec.rb
+++ spec/name_spec.rb
@@ -1,104 +1,76 @@
 # -*- coding: utf-8 -*-
 require_relative 'spec_helper'
 
-describe Gimei do
+describe Gimei::Name do
   describe '.male' do
-    before { @name = Gimei.male }
+    before { @name = Gimei::Name.male }
 
-    it '男性の名前が返ること' do
+    it 'Gimei::Name オブジェクトが返ること' do
+      @name.must_be_instance_of Gimei::Name
+    end
+
+    it '#male? が true を返すこと' do
       @name.male?.must_equal true
     end
   end
 
   describe '.female' do
-    before { @name = Gimei.female }
+    before { @name = Gimei::Name.female }
 
-    it '女性の名前が返ること' do
-      @name.female?.must_equal true
+    it 'Gimei::Name オブジェクトが返ること' do
+      @name.must_be_instance_of Gimei::Name
     end
-  end
 
-  describe '#kanji' do
-    before { @name = Gimei.new }
-
-    it '全角文字とスペースが返ること' do
-      @name.kanji.must_match /\A[#{Moji.zen}\s]+\z/
+    it '#female? が true を返すこと' do
+      @name.female?.must_equal true
     end
   end
 
   describe '.kanji' do
     it '全角文字とスペースが返ること' do
-      Gimei.kanji.must_match /\A[#{Moji.zen}\s]+\z/
-    end
-  end
-
-  describe '#hiragana' do
-    before { @name = Gimei.new }
-
-    it 'ひらがなとスペースが返ること' do
-      @name.hiragana.must_match /\A[\p{hiragana}\s]+\z/
+      Gimei::Name.kanji.must_match /\A[#{Moji.zen}\s]+\z/
     end
   end
 
   describe '.hiragana' do
     it 'ひらがなとスペースが返ること' do
-      Gimei.hiragana.must_match /\A[\p{hiragana}\s]+\z/
-    end
-  end
-
-  describe '#katakana' do
-    before { @name = Gimei.new }
-
-    it 'カタカナとスペースが返ること' do
-      @name.katakana.must_match /\A[\p{katakana}\s]+\z/
+      Gimei::Name.hiragana.must_match /\A[\p{hiragana}\s]+\z/
     end
   end
 
   describe '.katakana' do
     it 'カタカナとスペースが返ること' do
-      Gimei.katakana.must_match /\A[\p{katakana}\s]+\z/
+      Gimei::Name.katakana.must_match /\A[\p{katakana}\s]+\z/
     end
   end
 
-  describe '.first and .last' do
-    before do
-      unless self.class.const_defined?(:DATA_FILES)
-        DATA_FILES = YAML.load_file(File.expand_path(File.join('..', '..', 'lib', 'data', 'names.yml'), __FILE__))
-      end
-      @last_names   = DATA_FILES['last_name'].map { |df| df[0] }
-      @first_names  = DATA_FILES['first_name']['male'].map { |male| male[0] }
-      @first_names +=
-        DATA_FILES['first_name']['female'].map { |female| female[0] }
-      @gimei_first  = Gimei.first
-      @gimei_last   = Gimei.last
-    end
-
-    describe '.first' do
-      it '苗字が含まれていないこと' do
-        @last_names.include?(@gimei_first.kanji).must_equal false
-      end
-
-      it '名前が含まれていること' do
-        @first_names.include?(@gimei_first.kanji).must_equal true
-      end
-    end
-
-    describe '.last' do
-      it '苗字が含まれていること' do
-        @last_names.include?(@gimei_last.kanji).must_equal true
-      end
-
-      it '名前が含まれていないこと' do
-        @first_names.include?(@gimei_last.kanji).must_equal false
-      end
-    end
-  end
-
-  describe '#to_s' do
-    before { @name = Gimei.new }
-
+  describe '#kanji' do
     it '全角文字とスペースが返ること' do
-      @name.to_s.must_match /\A[#{Moji.zen}\s]+\z/
+      Gimei::Name.new.kanji.must_match /\A[#{Moji.zen}\s]+\z/
+    end
+  end
+
+  describe '#hiragana' do
+    it 'ひらがなとスペースが返ること' do
+      Gimei::Name.new.hiragana.must_match /\A[\p{hiragana}\s]+\z/
+    end
+  end
+
+  describe '#katakana' do
+    it 'カタカナとスペースが返ること' do
+      Gimei::Name.new.katakana.must_match /\A[\p{katakana}\s]+\z/
+    end
+  end
+
+  describe '#first' do
+    it 'Gimei::Name::First オブジェクトが返ること' do
+      Gimei::Name.new.first.must_be_instance_of Gimei::Name::First
+    end
+  end
+
+  describe '#last' do
+    it 'Gimei::Name::Last オブジェクトが返ること' do
+      Gimei::Name.new.last.must_be_instance_of Gimei::Name::Last
     end
   end
 end
@@ -107,7 +79,11 @@ describe Gimei::Name::First do
   describe '.male' do
     before { @name = Gimei::Name::First.male }
 
-    it '男性の名前が返ること' do
+    it 'Gimei::Name::First オブジェクトが返ること' do
+      @name.must_be_instance_of Gimei::Name::First
+    end
+
+    it '#male? が true を返すこと' do
       @name.male?.must_equal true
     end
   end
@@ -115,40 +91,36 @@ describe Gimei::Name::First do
   describe '.female' do
     before { @name = Gimei::Name::First.female }
 
-    it '女性の名前が返ること' do
+    it 'Gimei::Name::First オブジェクトが返ること' do
+      @name.must_be_instance_of Gimei::Name::First
+    end
+
+    it '#female? が true を返すこと' do
       @name.female?.must_equal true
     end
   end
 
   describe '#kanji' do
-    before { @name = Gimei::Name::First.new }
-
     it '全角文字が返ること' do
-      @name.kanji.must_match /\A#{Moji.zen}+\z/
+      Gimei::Name::First.new.kanji.must_match /\A#{Moji.zen}+\z/
     end
   end
 
   describe '#hiragana' do
-    before { @name = Gimei::Name::First.new }
-
     it 'ひらがなが返ること' do
-      @name.hiragana.must_match /\A\p{hiragana}+\z/
+      Gimei::Name::First.new.hiragana.must_match /\A\p{hiragana}+\z/
     end
   end
 
   describe '#katakana' do
-    before { @name = Gimei::Name::First.new }
-
     it 'カタカナが返ること' do
-      @name.katakana.must_match /\A\p{katakana}+\z/
+      Gimei::Name::First.new.katakana.must_match /\A\p{katakana}+\z/
     end
   end
 
   describe '#to_s' do
-    before { @name = Gimei::Name::First.new }
-
     it '全角文字が返ること' do
-      @name.to_s.must_match /\A#{Moji.zen}+\z/
+      Gimei::Name::First.new.to_s.must_match /\A#{Moji.zen}+\z/
     end
   end
 end
