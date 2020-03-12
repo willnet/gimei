515dac31c976512243f028387fb20f0aebecdd39
willnet <netwillnet@gmail.com>
2020-03-12 17:45:57 +0900

    define_unique_method add singleton methods instead of instance methods

--- lib/gimei/unique_generator.rb
+++ lib/gimei/unique_generator.rb
@@ -6,25 +6,18 @@ class Gimei
       def previous_results
         @previous_results ||= Hash.new { |hash, key| hash[key] = Set.new }
       end
-
-      def define_unique_method(method_name, previous_result_key = method_name)
-        define_method method_name do |*args|
-          max_retries.times do
-            result = Gimei.public_send(method_name, *args)
-
-            next if previous_results[previous_result_key].include?(result.to_s)
-
-            previous_results[previous_result_key] << result.to_s
-            return result
-          end
-
-          raise RetryLimitExceeded, "Retry limit exceeded for #{method_name}"
-        end
-      end
     end
 
     def initialize(max_retries)
       @max_retries = max_retries
+
+      %i[name last first address prefecture city town].each do |method_name|
+        define_unique_method(method_name)
+      end
+
+      %i[male female kanji hiragana katakana romaji].each do |method_name|
+        define_unique_method(method_name, :name)
+      end
     end
 
     def previous_results
@@ -39,12 +32,19 @@ class Gimei
       end
     end
 
-    %i[name last first address prefecture city town].each do |method_name|
-      define_unique_method(method_name)
-    end
+    def define_unique_method(method_name, previous_result_key = method_name)
+      define_singleton_method method_name do |*args|
+        max_retries.times do
+          result = Gimei.public_send(method_name, *args)
 
-    %i[male female kanji hiragana katakana romaji].each do |method_name|
-      define_unique_method(method_name, :name)
+          next if previous_results[previous_result_key].include?(result.to_s)
+
+          previous_results[previous_result_key] << result.to_s
+          return result
+        end
+
+        raise RetryLimitExceeded, "Retry limit exceeded for #{method_name}"
+      end
     end
 
     private
