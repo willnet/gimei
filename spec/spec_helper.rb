f5e0ad43d8c1d028d958a43473863d4e75c35ee0
willnet <netwillnet@gmail.com>
2023-07-02 16:13:46 +0900

    simplecovを追加
    coveralls gemを削除したので、明示的にsimplecovを依存関係に入れる必要があった

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,8 @@ lib = File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require 'bundler/setup'
-
+require 'simplecov'
+SimpleCov.start
 require 'gimei'
 require 'minitest/autorun'
 
